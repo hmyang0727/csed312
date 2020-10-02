e67aea533b95a36b4018e79f6fb9adad0c8c8acc
hmyang0727 <heeminid@gmail.com>
2020-10-02 00:07:18 -0700

    Modify: Modified error which was caused by not adding list initialization for owning_lock.

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -482,6 +482,7 @@ init_thread (struct thread *t, const char *name, int priority)
   t->priority = priority;
   t->original_priority = priority;
   t->magic = THREAD_MAGIC;
+  list_init(&t->owning_lock);
 
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
